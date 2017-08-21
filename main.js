73aed049ae67f2b67ba0b960d59037b438c827ca
HyunUk Jeong <hjeong326@gmail.com>
2017-08-21 02:19:22 -0500

    More accurate API query

--- main.js
+++ main.js
@@ -9,12 +9,14 @@ $("document").ready(function() {
 	$searchForm.submit(function(event) {
 		// $checker.hide();
 		// var text = document.getElementById("text-input").value;
-		var text = document.querySelector("#text-input").value;
+		var text = document.querySelector("#text-input");
 		$checker
-			.text("Your searched for: " + text)
+			.text("Your searched for: " + text.value)
 			.fadeIn(700)
 			.fadeOut(700);
-		query(text);
+		// query(text.value);
+		query2(text.value);
+		text.value = "";  // empty the text field
 		return false;  // same as event.preventDefault(); NOTE: page reloads when a form is submitted.
 
 	});
@@ -25,6 +27,7 @@ $("document").ready(function() {
 			type: 'GET',
 			dataType: 'json',
 			url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch='+searchTerm+'&srlimit=3&srprop=snippet&srinfo=Null&format=json&origin=*',
+			// url: 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles='+searchTerm+'exsentences=3&format=json&origin=*',
 			success: function(data) {
 				alert("success");
 				var searches = data.query.search;
@@ -34,6 +37,31 @@ $("document").ready(function() {
 				// });
 				// $jsonChecker.html(tempStr);
 				$jsonChecker.html(JSON.stringify(searches));
+
+			},
+			error: function(jqXHR, status, err) {
+				alert("error");
+				$jsonChecker.html(JSON.stringify(jqXHR) + "\n\n\n" + status + "\n\n\n" + err);
+			}
+		});
+	}
+
+	function query2(searchTerm) {
+		$.ajax({
+			type: 'GET',
+			dataType: 'json',
+			// url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch='+searchTerm+'&srlimit=3&srprop=snippet&srinfo=Null&format=json&origin=*',
+			url: 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&pageids=170570|3066589|38569806&exsentences=2&exintro=True&exlimit=5&format=json&origin=*',
+			success: function(data) {
+				alert("success");
+				var searches = data;
+				var tempStr = "";
+				// searches.forEach(function(item) {
+				// 	tempStr += item.title + "<br>" + item.snippet + "<br>";
+				// });
+				// $jsonChecker.html(tempStr);
+				$jsonChecker.html(JSON.stringify(searches));
+
 			},
 			error: function(jqXHR, status, err) {
 				alert("error");
